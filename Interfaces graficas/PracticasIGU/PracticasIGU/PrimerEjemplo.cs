acd5686b265c2328d26bfafa7c24cfe2d73c9c89
Francisco Pinto Santos <franpintosantos@usal.es>
2018-09-21 12:21:49 +0200

    Interfaces graficas - practica 0

---
+++ Interfaces graficas/PracticasIGU/PracticasIGU/PrimerEjemplo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+namespace Proyectos.ElSegundo
+{
+    class Basica
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Window win = new Window();
+            win.Title = "Primer Programa";
+            win.Show();
+
+            Label etiqueta = new Label();
+            etiqueta.Content = "Hola Mundo";
+
+            win.Content = etiqueta;
+
+            Console.WriteLine("Buenas mozo");
+
+            Application app = new Application();
+            app.Run();
+
+            Console.WriteLine("Adios mozo");
+        }
+
+    }
+}
